363ad601d97bfa3a9e2dc2d52d0d3a24f2934162
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-13 19:27:24 +0200

    Add validators lib

---
+++ chalicelib/lib/validators.py
@@ -0,0 +1,25 @@
+def validate_reason(reason: str, reasons: tuple) -> bool:
+    """Validates a given reason for an event
+    :param reason: str
+    :param reasons: tuple
+    :return: bool
+    """
+    if reason in reasons:
+        return True
+    else:
+        return False
+
+
+def validate_hours(hours: str) -> bool:
+    if round(float(hours)) > 8:
+        return False
+    elif round(float(hours)) < 0:
+        return False
+    else:
+        return True
+
+
+def validate_input(command: list) -> bool:
+    if len(command) < 3 or len(command) > 4:
+        return False
+    return True
