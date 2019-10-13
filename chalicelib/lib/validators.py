383f337f81e670a2f6fff5064424c5835b110ee6
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-10-13 20:22:33 +0200

    More clear logic in validators

--- chalicelib/lib/validators.py
+++ chalicelib/lib/validators.py
@@ -11,15 +11,24 @@ def validate_reason(reason: str, reasons: tuple) -> bool:
 
 
 def validate_hours(hours: str) -> bool:
-    if round(float(hours)) > 8:
+    try:
+        if round(float(hours)) > 8:
+            return False
+        elif round(float(hours)) < 0:
+            return False
+        else:
+            return True
+    except ValueError:
         return False
-    elif round(float(hours)) < 0:
-        return False
-    else:
-        return True
 
 
 def validate_command_len(command: list) -> bool:
-    if len(command) < 3 or len(command) > 4:
+    """
+    Force length of command to be of len 3 or 4
+    :param command: list
+    :return: bool
+    """
+    if len(command) == 3 or len(command) == 4:
+        return True
+    else:
         return False
-    return True
